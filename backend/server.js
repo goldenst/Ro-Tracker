a77dae76d95809b299a4a259af3b6001651f46f9
goldenst <jeff@goldenstatetow.com>
2022-08-14 20:27:06 -0500

    removed  app.get from server

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+
 
 // Routes
 app.use("/api/user", require("./routes/userRoutes"));
