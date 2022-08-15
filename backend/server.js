f66ce56012fe8e29e00191fd3d396fa39c183189
goldenst <jeff@goldenstatetow.com>
2022-08-14 20:10:42 -0500

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require ('path')
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -21,6 +22,18 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+  // set static build
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req,res) => res.sendFile(__dirname, '../' , 'frontend', 'build', 'index.html'))
+
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello!! Welcome to Open Ticket");
+  })
+}
 
 app.use(errorHandler);
 
