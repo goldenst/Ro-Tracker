ca034ea946868c44b05d669790cd03f52ca1a729
goldenst <jeff@goldenstatetow.com>
2022-08-15 17:11:23 -0500

    fixed server js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require ('path')
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -15,29 +15,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
 // Routes
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 // Serve Frontend
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // set static build
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
-//app.get('*', (req,res) => res.sendFile(__dirname, '../' , 'frontend', 'build', 'index.html'))
-app.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-})
-
-
-
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("Hello!! Welcome to Open Ticket");
-  })
+  });
 }
 
 app.use(errorHandler);
 
-//app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
