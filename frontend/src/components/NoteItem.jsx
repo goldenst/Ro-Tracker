ba8a6fca5780661a1e628ac6c72195d4ff2b44a6
goldenst <jeff@goldenstatetow.com>
2022-08-16 21:41:32 -0500

    updated ticket and note  from user.name to note.name

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-function NoteItem({note}) {
-  const {user} = useSelector((state) => state.auth)
-  const {note} = useSelector((state) => state.note)
-  
- 
-  
+function NoteItem({ note }) {
+  //const { user } = useSelector((state) => state.auth)
+  //const { note } = useSelector((state) => state.note)
+
+
+
   return (
-  <div className="note">
-    <h4>Note From: {note.name}</h4>
-    <p>{note.text}</p>
-    <div className="note-date" >
-      {new Date(note.createdAt).toLocaleString('en-us')}
+    <div className="note">
+      <h4>Note From: {note.name}</h4>
+      <p>{note.text}</p>
+      <div className="note-date" >
+        {new Date(note.createdAt).toLocaleString('en-us')}
+      </div>
     </div>
-  </div>
-    
+
   )
 }
 
